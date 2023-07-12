0b201973f159d3cfc0cd22624adfcde62ecd49d7
Muhammad Huzaifa Ali Qureshi <64558943+Huzaifaali7799@users.noreply.github.com>
2023-07-12 21:01:33 +0500

    Update data.js
    Adding images to data.js

--- Project_02_images_material/data.js
+++ Project_02_images_material/data.js
@@ -1,6 +1,14 @@
 const rentHouse = [
     {
         id: 1,
+        images: [
+          "House 1/home1-1.jpeg",
+          "House 1/home1-2.jpeg",
+          "House 1/home1-3.jpeg",
+          "House 1/home1-4.jpeg",
+          "House 1/home1-5.jpeg",
+          "House 1/home1-6.jpeg",
+        ],
         name: "House # 1",
         address: "DHA 9 City, Lahore, Pakistan",
         price: "$ 155,000 USD",
@@ -12,12 +20,18 @@ const rentHouse = [
             "Unfurnished",
             "Area: 10 marla"
         ],
-        description: [
-            "DHA 9 Housing Scheme A block 10 Marla New brand house available for sale Good location house near by hospital and market and masjid and LGS school and other facilities"
-        ]
+        description: "DHA 9 Housing Scheme A block 10 Marla New brand house available for sale Good location house near by hospital and market and masjid and LGS school and other facilities"
     },
     {
         id: 2,
+        images: [
+          "House 2/home2-1.jpeg",
+          "House 2/home2-2.jpeg",
+          "House 2/home2-3.jpeg",
+          "House 2/home2-4.jpeg",
+          "House 2/home2-5.jpeg",
+          "House 2/home2-6.jpeg",
+        ],
         name: "House # 2",
         address: "Bahria Town, Karachi, Pakistan",
         price: "$ 535,500 USD",
@@ -31,12 +45,16 @@ const rentHouse = [
             "Half-furnished",
             "Area: 20 marla"
         ],
-        description: [
-            "DHA 9 Housing Scheme A block 10 Marla New brand house available for sale Good location house near by hospital and market and masjid and LGS school and other facilities"
-        ]
+        description: "Bahria Town 20 Marla New brand house available for sale Good location house near by hospital and market and masjid and school, park and other facilities in fully future developed society"
     },
         {
           id: 3,
+          images: [
+          "House 3/home3-1.jpeg",
+          "House 3/home3-2.jpeg",
+          "House 3/home3-3.jpeg",
+          "House 3/home3-4.jpeg",
+        ],
           name: "House # 3",
           address: "Gulistan-e-Johar, Karachi, Pakistan",
           price: "$ 38,550 USD",
@@ -51,12 +69,17 @@ const rentHouse = [
             "West open Balcony",
             "Area: 950 sqr.feet"
           ],
-          description: [
-            "Apartment in installment (View phone number) Sqfeet VIP location near to park Gand masjid Gulistan-e-Johar karachi. You can find a Flat on the lower or upper levels, as suiting to your requirement. Buying property is a one-time life decision and we can help you with it. 950 Square Feet sized residential or commercial property is best for investment purposes. All your queries concerned with this property are welcome, so please contact us."
-          ]
+          description: "Apartment in installment (View phone number) Sqfeet VIP location near to park Gand masjid Gulistan-e-Johar karachi. You can find a Flat on the lower or upper levels, as suiting to your requirement. Buying property is a one-time life decision and we can help you with it. 950 Square Feet sized residential or commercial property is best for investment purposes. All your queries concerned with this property are welcome, so please contact us."
         },
         {
           id: 4,
+          images: [
+          "House 4/home4-1.jpeg",
+          "House 4/home4-2.jpeg",
+          "House 4/home4-3.jpeg",
+          "House 4/home4-4.jpeg",
+          "House 4/home4-5.jpeg",
+        ],
           name: "House # 4",
           address: "Blue Area, F-3, Islamabad, Pakistan",
           price: "$ 49,565 USD",
@@ -71,12 +94,16 @@ const rentHouse = [
             "Garden View Balcony",
             "Area: 1,100 sqr.feet"
           ],
-          description: [
-            "The Flat is well designed to accommodate the family and their guests. When it comes to housing, location is everything and we have quite a many options regarding that. The perfect 1,1000 Square Feet listing for your property needs are right within your reach."
-          ]
+          description: "The Flat is well designed to accommodate the family and their guests. When it comes to housing, location is everything and we have quite a many options regarding that. The perfect 1,1000 Square Feet listing for your property needs are right within your reach."
         },
         {
           id: 5,
+          images: [
+          "House 5/home5-1.jpeg",
+          "House 5/home5-2.jpeg",
+          "House 5/home5-3.jpeg",
+          "House 5/home5-4.jpeg",
+        ],
           name: "House # 5",
           address: "Saadi Town, Faisalabad, Pakistan",
           price: "$ 71,961 USD",
@@ -90,12 +117,17 @@ const rentHouse = [
             "Car Parking",
             "Area: 120 sqr.yards"
           ],
-          description: [
-            "Saadi Town Brand New Project Banglow Double Story 120 Sq Yards Bedrooms with attach baths Drawing Lounge open American Kitchen Washing area Huge Terrace Tiles Flooring Fancy lights Complete Boundary wall project Daily Sweet line Water No Load Shedding gas Security Guards Masjid Park Pure Family Environment Main Road approach Ideal Location"
-          ]
+          description: "Saadi Town Brand New Project Banglow Double Story 120 Sq Yards Bedrooms with attach baths Drawing Lounge open American Kitchen Washing area Huge Terrace Tiles Flooring Fancy lights Complete Boundary wall project Daily Sweet line Water No Load Shedding gas Security Guards Masjid Park Pure Family Environment Main Road approach Ideal Location"
         },
         {
           id: 6,
+          images: [
+          "House 6/home6-1.jpeg",
+          "House 6/home6-2.jpeg",
+          "House 6/home6-3.jpeg",
+          "House 6/home6-4.jpeg",
+          "House 6/home6-5.jpeg",
+        ],
           name: "House # 6",
           address: "Bahria Town, Karachi, Pakistan",
           price: "$ 91,788 USD",
@@ -111,9 +143,7 @@ const rentHouse = [
             "A roof top balcony",
             "Area: 120 sqr.yards"
           ],
-          description: [
-            "The Best Design As Per Your Desire And Your Dream House a Well Designed House In A Posh Location Is All You Need To Live Your Best Life. Right Now Is The Time To Buy This Property. This Property In Bahria Town is an Opportunity For The Investors And Buyers Alike, So Make The Most Out Of It. Bahria Town S Unique Features Bahria Town Is A Secure Community 24 7 Electricity Available Under Ground Electricity WiringGrand Jamia Masjid Petrol Pump Feel Free To Contact For Further Details"
-          ]
+          description: "The Best Design As Per Your Desire And Your Dream House a Well Designed House In A Posh Location Is All You Need To Live Your Best Life. Right Now Is The Time To Buy This Property. This Property In Bahria Town is an Opportunity For The Investors And Buyers Alike, So Make The Most Out Of It. Bahria Town S Unique Features Bahria Town Is A Secure Community 24 7 Electricity Available Under Ground Electricity WiringGrand Jamia Masjid Petrol Pump Feel Free To Contact For Further Details"
         },
       ]
     
